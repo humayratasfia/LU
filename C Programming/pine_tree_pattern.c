00222594837e6ea5b67b131838a783843532676a
Humayra Tasfia <87924957+humayratasfia@users.noreply.github.com>
2026-01-02 02:01:39 +0600

    Add pine tree pattern printing program
    This program prints a pine tree pattern based on user input for the height of the tree.

---
+++ C Programming/pine_tree_pattern.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int gap = n-1;
+    int star = 1;
+    //Upper part of the tree
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=gap;j>=1;j--)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=star;j++)
+        {
+            printf("*");
+        }
+        gap--;
+        star+=2;
+        printf("\n");
+    }
+    //Lower part of the tree
+    for(int i=1;i<=n-1;i++)
+    {
+        for(int j=1;j<=n-2;j++)
+        {
+            printf(" ");
+        }
+        for(int j=1;j<=3;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
