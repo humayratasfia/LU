4e69d44257cc640da3dd540919f940f02832a08f
Humayra Tasfia <87924957+humayratasfia@users.noreply.github.com>
2026-03-02 13:40:55 +0600

    Bubble sort Optimized added

---
+++ CSE-2111 & 2112 (Data Structure)/From Lectures/Algorithm/Bubble_Sort_Optimized.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Size: ";
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    cout << "Inserted Array: ";
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "Bubble Sort: " << endl;
+    for(int i=0;i<n-1;i++)
+    {
+        int swapped = 0;
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                swap(a[j], a[j+1]);
+                swapped++;
+            }
+        }
+
+        cout << "Pass " << i+1 << ": ";
+        for(int j=0;j<n;j++)
+        {
+            cout << a[j] << " ";
+        }
+        cout << endl;
+
+        if(swapped == 0) // In a pass if no swap happens then the loop breaks.
+        {
+            break;
+        }
+    }
+
+    cout << "Updated Array: ";
+    for(int i=0;i<n;i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
