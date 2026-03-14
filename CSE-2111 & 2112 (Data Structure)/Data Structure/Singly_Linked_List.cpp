cab03da3b36c37dcbe310993c36b4a40162b0bbb
Humayra Tasfia <87924957+humayratasfia@users.noreply.github.com>
2026-03-15 04:18:08 +0600

    update

---
+++ CSE-2111 & 2112 (Data Structure)/Data Structure/Singly_Linked_List.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    Node* head;
+    Node* first, second, third;
+
+    first->data = 10;
+    first->next = &second;
+    return 0;
+}
